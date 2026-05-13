3e90050251eeba17bfed91dd83776f9ad28618c7
Светлана <fox.2025.owl@gmail.com>
2026-05-13 19:18:09 +1000

    перенос файлов tailwidn.config.js и postcss.config.js в папку appwriteChat

---
+++ appwritechat/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    content: [
+        "./index.html", 
+        "./src/**/*.{js, ts, jsx, tsx, html",
+        "./public/**/*.html"
+    ],
+    theme: {
+        extend: {},
+    },
+    darkMode: "class",
+};
