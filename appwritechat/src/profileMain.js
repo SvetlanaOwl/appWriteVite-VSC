975abce5534a0b3131eacf2f131a6270fe05193c
Светлана <fox.2025.owl@gmail.com>
2026-05-17 12:48:33 +1000

    работа с файлом профиля

---
+++ appwritechat/src/profileMain.js
@@ -0,0 +1,13 @@
+import './css/utils.css' 
+import { themeSwitcher } from './js/themeSwitcher.js'
+import { createHeader } from './js/header.js'
+import { createProfileCard } from './js/profile.js'
+import { createFooter } from './js/footer.js'
+import { updateAccountName } from './js/appwriteUtils.js'
+
+createHeader();
+themeSwitcher();
+createProfileCard();
+createFooter();
+updateAccountName();
+
